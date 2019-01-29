d531070456544f58d3c0f7e13dea6fc3f0628dbd
jasonromulus <pumcome@me.com>
2019-01-29 11:49:14 -0800

    My rearrange now works with help from Alan

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my Tweet generator poject for CS 1.2
---
+++ rearrange.py
@@ -0,0 +1,24 @@
+import random
+import sys
+
+def rearrange(list):
+    print('list:', list)
+    new_list = []
+    counter = len(list)
+    while counter > 0:
+        random_index = random.randrange(len(list))
+        chosen_word = list.pop(random_index)
+        print('word:', chosen_word)
+        new_list.append(chosen_word)
+        # list.remove(chosen_word)
+        counter -= 1
+    print('new_list:', new_list)
+    results = " ".join(new_list)
+    print(results)
+
+if __name__ == '__main__':
+    # words = ['apple', 'orange', 'banana']
+    # rearrange(words)
+
+    arguments = sys.argv[1:]
+    rearrange(arguments)
