1f20dd86afdf141984b6f86abba06cf78d88f43d
jasonromulus <pumcome@me.com>
2019-02-06 14:57:01 -0800

    Can't get these files to open properly.

--- histogram.py
+++ histogram.py
@@ -4,3 +4,10 @@ def histogram():
         myList = f.read().split(" ")
         print(myList)
 histogram()
+
+# def load_spies():
+#     with open('villain.txt', 'r') as f:
+#         read_data = f.read().split(" ")
+#     # f.closed
+#         print(read_data)
+# load_spies()
