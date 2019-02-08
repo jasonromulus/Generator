c14d2b7779ef5374b615a5bdb26efabcad5c63a6
jasonromulus <pumcome@me.com>
2019-02-07 23:44:03 -0800

    Trying to figure out why this won't work anymore.

--- histogram.py
+++ histogram.py
@@ -1,13 +1,13 @@
-def histogram():
-    print("hello")
-    with open('villain.txt', 'r') as f:
-        myList = f.read().split(" ")
-        print(myList)
-histogram()
-
-# def load_spies():
+# def histogram():
+#     print("hello")
 #     with open('villain.txt', 'r') as f:
-#         read_data = f.read().split(" ")
-#     # f.closed
-#         print(read_data)
-# load_spies()
+#         myList = f.read().split(" ")
+#         print(myList)
+# histogram()
+
+def load_spies():
+    with open('villain.txt', 'r') as f:
+        read_data = f.read().split(" ")
+    # f.closed
+        print(read_data)
+load_spies()
